7993d4bd9bda0068536a11d9e7d6bc6cc8dfbcb2
Cycloctane <Cycloctane@outlook.com>
2025-02-11 13:39:48 +0800

    use concurrency in dlnafind

--- cmd/dlnafind/dlnafind.go
+++ cmd/dlnafind/dlnafind.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sync"
 
 	"github.com/Cycloctane/dlnapass/internal/upnp"
+	"github.com/koron/go-ssdp"
 )
 
 const defaultSearchSec = 1
@@ -19,21 +21,36 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("Found %d devices:\n\n", len(devices))
-	for i, d := range devices {
-		fmt.Printf("Device #%d: %s\n", i+1, d.USN)
-		fmt.Printf("\t[+] Server: %s\n", d.Server)
-		fmt.Printf("\t[+] Description URL: %s\n", d.Location)
-		desc, err := upnp.GetDesc(d.Location)
-		if err != nil {
-			fmt.Print("\t[!] Error: Cannot connect to target device\n\n")
-			continue
-		}
-		fmt.Printf("\t[+] DeviceType: %s\n", desc.Device.DeviceType)
-		fmt.Printf("\t[+] ModelName: %s\n", desc.Device.ModelName)
-		fmt.Printf("\t[+] FriendlyName: %s\n", desc.Device.FriendlyName)
-		for j, s := range desc.Device.ServiceList {
-			fmt.Printf("\t[+] Service #%d: %s\n", j+1, s.ServiceType)
-		}
-		fmt.Print("\n")
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	i := 0
+	for _, d := range devices {
+		wg.Add(1)
+		go func(d ssdp.Service) {
+			defer wg.Done()
+			desc, err := upnp.GetDesc(d.Location)
+			mu.Lock()
+			defer mu.Unlock()
+
+			fmt.Printf(
+				"Device #%d: %s\n\t[+] Server: %s\n\t[+] Description URL: %s\n",
+				i+1, d.USN, d.Server, d.Location,
+			)
+			i++
+			if err != nil {
+				fmt.Print("\t[!] Error: Cannot connect to target device\n\n")
+				return
+			}
+			fmt.Printf(
+				"\t[+] DeviceType: %s\n\t[+] ModelName: %s\n\t[+] FriendlyName: %s\n",
+				desc.Device.DeviceType, desc.Device.ModelName, desc.Device.FriendlyName,
+			)
+			for j, s := range desc.Device.ServiceList {
+				fmt.Printf("\t[+] Service #%d: %s\n", j+1, s.ServiceType)
+			}
+			fmt.Print("\n")
+		}(d)
 	}
+	wg.Wait()
 }
